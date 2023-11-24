404622da9dd95f2a7e1d243c3d3113f0f9339c3b
2joopiter <veryberrylike3@gmail.com>
2023-11-24 11:19:24 +0900

    footer styleing

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,5 +1,27 @@
 import './Footer.scss';
+import { TiSocialInstagram } from 'react-icons/ti';
+import { FaSquareXTwitter } from 'react-icons/fa6';
+import { IoLogoYoutube } from 'react-icons/io';
+
+// npm i react-icons (기본 아이콘 설치 가능)
 
 export default function Footer() {
-	return <div className='Footer'>Footer</div>;
+	return (
+		<footer className='Footer'>
+			<h1>Home</h1>
+			<p>2023 home &copy; All Right Reserved. </p>
+
+			<ul>
+				<li>
+					<TiSocialInstagram />
+				</li>
+				<li>
+					<FaSquareXTwitter />
+				</li>
+				<li>
+					<IoLogoYoutube />
+				</li>
+			</ul>
+		</footer>
+	);
 }
