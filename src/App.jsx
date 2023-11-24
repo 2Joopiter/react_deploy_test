556308fb01bf7cf0d9d18b2835e8326488e4a4b5
2joopiter <veryberrylike3@gmail.com>
2023-11-24 09:49:11 +0900

    모든 컴포넌트 App에서 호출

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import Header from './components/common/header/Header';
+import MainWrap from './components/main/mainWrap/MainWrap';
+import Community from './components/sub/community/Community';
+import Contact from './components/sub/contact/Contact';
+import Department from './components/sub/department/Department';
+import Gallery from './components/sub/gallery/Gallery';
+import Members from './components/sub/members/Members';
+import Youtube from './components/sub/youtube/Youtube';
+import Footer from './components/common/footer/Footer';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Header />
+			<MainWrap />
+			<Community />
+			<Contact />
+			<Department />
+			<Gallery />
+			<Members />
+			<Youtube />
+			<Footer />
+		</>
 	);
 }
 
