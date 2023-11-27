37d7b502e44e3da8deb98cb386e030175b184d33
2joopiter <veryberrylike3@gmail.com>
2023-11-27 13:14:51 +0900

    첫번째 문자만 자동으로 대문자로 변경하는 로직 적용

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -3,6 +3,12 @@ import Layout from '../../common/layout/Layout';
 import './Department.scss';
 
 export default function Department() {
+	//const test = 'abcdef';
+	//console.log(test.charAt(0)); = a를 반환
+	//console.log(test.slice(1, 3)); // bc를 반환
+	//console.log(test.slice(1)); // bcdef 반환
+	//console.log(test.toUpperCase()); // 전체 대문자 반환
+
 	const [MemberData, setMemberData] = useState([]);
 	const path = process.env.PUBLIC_URL; // public 폴더까지의 경로를 구해주는 구문
 	const [MemberTit, setMemberTit] = useState('');
@@ -26,7 +32,7 @@ export default function Department() {
 	return (
 		<Layout title={'Department'}>
 			<section className='memberBox'>
-				<h2>{MemberTit}</h2>
+				<h2>{`${MemberTit.charAt(0).toUpperCase() + MemberTit.slice(1g)}`}</h2>
 				{MemberData.map((member, idx) => {
 					return (
 						<article key={member + idx}>
@@ -59,8 +65,8 @@ export default function Department() {
 	문자열 관련 내장 메서드
 	전체문자열.charAt(순서): 전체 문자열에서 해당 순서의 문자값만 반환
 	전체문자열.slice(순서1, 순서2): 전체 문자열에서 해당 순서1부터 순서2 위치까지 문자를 잘라서 반환
-	전체문자열.upperCase(): 문자열을 전체 대문자로 반환
-	전체문자열.lowerCase(): 문자열을 전체 소문자로 반환
+	전체문자열.toUpperCase(): 문자열을 전체 대문자로 반환
+	전체문자열.toLowerCase(): 문자열을 전체 소문자로 반환
 	전체문자열.split(구분자): 전체문자열을 구분자를 기준으로 나눠서 배열로 반환
 	배열.join('구분자): 각 배열값을 구분자로 이어붙이면서 하나의 문자열로 반환
 
