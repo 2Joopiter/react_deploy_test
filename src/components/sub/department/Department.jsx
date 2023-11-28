194408bf186ece3617b6e61a149b58278d5e2c93
2joopiter <veryberrylike3@gmail.com>
2023-11-28 11:40:16 +0900

    title 함수 기능을 combined 함수에 통합

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -7,16 +7,7 @@ export default function Department() {
 	const [MemberData, setMemberData] = useState([]);
 	const [MemberTit, setMemberTit] = useState('');
 	const path = useRef(process.env.PUBLIC_URL);
-	const changeTitle = useCustomText('title');
-
-	const test1 = 'our-members';
-	console.log(test1.split('-')); // ['our','member']로 - 를 기준으로 단어를 나눠줌
-	const [forward, backward] = test1.split('-');
-	console.log(changeTitle(forward) + '' + changeTitle(backward)); // 'Our Members' 출력
-
 	const combinedTitle = useCustomText('combined');
-	const test2 = 'our-members-score';
-	console.log(combinedTitle(test2, '-'));
 
 	const fetchDepartment = () => {
 		fetch(`${path.current}/DB/department.json`)
@@ -35,7 +26,7 @@ export default function Department() {
 	return (
 		<Layout title={'Department'}>
 			<section className='memberBox'>
-				<h2>{changeTitle(MemberTit)}</h2>
+				<h2>{combinedTitle(MemberTit, '-')}</h2>
 				{MemberData.map((member, idx) => {
 					return (
 						<article key={member + idx}>
--- src/hooks/useText.js
+++ src/hooks/useText.js
@@ -12,11 +12,10 @@ export function useSplitText() {
 }
 
 export function useCustomText(type) {
-	if (type === 'title') {
-		return (txt) => {
-			return txt.charAt(0).toUpperCase() + txt.slice(1);
-		};
-	}
+	const toUpperText = (txt) => {
+		return txt.charAt(0).toUpperCase() + txt.slice(1);
+	};
+
 	if (type === 'shorten') {
 		return (txt, len = 100) => {
 			if (txt.length > len) {
@@ -28,11 +27,11 @@ export function useCustomText(type) {
 	}
 	if (type === 'combined') {
 		return (txt, spc) => {
-			const resultText = txt // 원본 텍스트를 가져옴
-				.split(spc) // split으로 받은 텍스트를 구분자로 분리해 배열로 반환
-				.map((data) => data.charAt(0).toUpperCase() + data.slice(1)) // 배열값을 map으로 반복돌며 첫글자만 대문자로 반환해서 새로운 배열로 반환
-				.join(' '); // 새롭게 반환된 배열을 다시 빈칸을 구분자로(기준으로) 하나의 문자열로 이어붙여줌
-			return resultText; // 위에서 만들어진 문자값을 최종적으로 반환
+			const resultText = txt
+				.split(spc)
+				.map((data) => toUpperText(data))
+				.join(' ');
+			return resultText;
 		};
 	}
 }
