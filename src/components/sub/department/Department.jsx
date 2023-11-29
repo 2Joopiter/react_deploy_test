80a93477782ea08582b56e43548e4f449f8f8ba0
2joopiter <veryberrylike3@gmail.com>
2023-11-29 10:36:11 +0900

    history json에서 중첩 구조 데이터 추출

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -9,6 +9,19 @@ export default function Department() {
 	const path = useRef(process.env.PUBLIC_URL);
 	const combinedTitle = useCustomText('combined');
 
+	const [HistoryTit, setHistoryTit] = useState('');
+	const [HistoryData, setHistoryData] = useState([]);
+
+	const fetchHistory = () => {
+		fetch(`${path.current}/DB/history.json`)
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json);
+				setHistoryTit(Object.keys(json)[0]);
+				setHistoryData(Object.values(json)[0]);
+			});
+	};
+
 	const fetchDepartment = () => {
 		fetch(`${path.current}/DB/department.json`)
 			.then((data) => data.json())
@@ -22,9 +35,31 @@ export default function Department() {
 	useEffect(() => {
 		fetchDepartment();
 	}, []);
+	useEffect(() => {
+		fetchHistory();
+	}, []);
 
 	return (
 		<Layout title={'Department'}>
+			<section className='historyBox'>
+				<h2>{combinedTitle(HistoryTit)}</h2>
+				<div className='con'>
+					{/* {2016: 배열} */}
+					{HistoryData.map((history, idx) => {
+						return (
+							<article key={history + idx}>
+								<h3>{Object.keys(history)[0]}</h3>
+								<ul>
+									{Object.values(history)[0].map((list, idx) => {
+										return <li key={list + idx}>{list}</li>;
+									})}
+								</ul>
+							</article>
+						);
+					})}
+				</div>
+			</section>
+
 			<section className='memberBox'>
 				<h2>{combinedTitle(MemberTit)}</h2>
 				<div className='con'>
