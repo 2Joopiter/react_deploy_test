9d6c2edec25db961c0fe9caff1a6286bcb67a615
2joopiter <veryberrylike3@gmail.com>
2023-11-30 10:40:00 +0900

    promise then을 async await으로 고도화

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -6,18 +6,19 @@ export default function Youtube() {
 	const [Vids, setVids] = useState([]);
 	console.log(Vids);
 
-	const fetchYoutube = () => {
+	const fetchYoutube = async () => {
 		const api_key = 'AIzaSyBCqSk4zIkhEpjHpiWBU8U5ZbT7HiSItqc';
 		const pid = 'PL7mCE55Wm-dixtLfubWtcGFp5lge1UHKA';
 		const num = 10;
 		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-		fetch(baseURL)
-			.then((data) => {
-				return data.json();
-			})
-			.then((json) => {
-				setVids(json.items);
-			});
+
+		try {
+			const data = await fetch(baseURL);
+			const json = await data.json();
+			setVids(json.items);
+		} catch (err) {
+			console.error(err);
+		}
 	};
 
 	useEffect(() => {
