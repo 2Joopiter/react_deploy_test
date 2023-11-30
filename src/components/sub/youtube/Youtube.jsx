967def86eb6705f734b4f7894b05d5d8e93cb5ab
2joopiter <veryberrylike3@gmail.com>
2023-11-30 11:00:33 +0900

    state 배열값을 반복돌면서 jsx 출력하고 문자열을 커스텀훅으로 가공

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,8 +1,11 @@
 import Layout from '../../common/layout/Layout';
 import './Youtube.scss';
 import { useState, useEffect } from 'react';
+import { useCustomText } from '../../../hooks/useText';
 
 export default function Youtube() {
+	const customText = useCustomText('combined');
+	const shortenText = useCustomText('shorten');
 	const [Vids, setVids] = useState([]);
 	console.log(Vids);
 
@@ -27,7 +30,27 @@ export default function Youtube() {
 
 	return (
 		<Layout title={'Youtube'}>
-			<p>Youtube 전용 컨텐츠</p>
+			{Vids.map((data, idx) => {
+				const [date, time] = data.snippet.publishedAt.split('T');
+
+				return (
+					<article key={data.id}>
+						<h2>{shortenText(data.snippet.title, 50)}</h2>
+
+						<div className='txt'>
+							<p>{shortenText(data.snippet.description, 250)}</p>
+							<div className='infoBox'>
+								<span>{customText(date, '.')}</span>
+								<em>{time.split('Z')[0]}</em>
+							</div>
+						</div>
+
+						<div className='pic'>
+							<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
