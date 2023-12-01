895a9aae5b6dffe876c4b920f6c0750ffe5c60b9
2joopiter <veryberrylike3@gmail.com>
2023-12-01 12:04:52 +0900

    이미지 엑박시 대체 이미지 출력

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -6,14 +6,15 @@ export default function Gallery() {
 	const [Pics, setPics] = useState([]);
 
 	const fetchFlickr = async () => {
-		const flickr_api = '96b263310d877718514c269c64e06a63';
+		const flickr_api = process.env.REACT_APP_FLICKR_API;
 		const baseURL = 'https://www.flickr.com/services/rest/?method=';
 		const method_interest = 'flickr.interestingness.getList';
-		const num = 500;
+		const num = 30;
 		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
 
 		const data = await fetch(resultURL);
 		const json = await data.json();
+		console.log(json.photos.photo);
 		setPics(json.photos.photo);
 	};
 
@@ -23,13 +24,30 @@ export default function Gallery() {
 
 	return (
 		<Layout title={'Gallery'}>
-			{Pics.map((pic, idx) => {
-				return (
-					<article key={pic.id}>
-						<h2>{pic.title}</h2>
-					</article>
-				);
-			})}
+			<div className='frame'>
+				{Pics.map((pic, idx) => {
+					return (
+						<article key={pic.id}>
+							<div className='pic'>
+								<img
+									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+									alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+								/>
+							</div>
+							<h2>{pic.title}</h2>
+
+							<div className='profile'>
+								<img
+									src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+									alt='사용자 프로필 이미지'
+									onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
+								/>
+								<span>{pic.owner}</span>
+							</div>
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
