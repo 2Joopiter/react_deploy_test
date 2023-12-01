007f677b1f871cbaf549092e9e1f345c4416c222
2joopiter <veryberrylike3@gmail.com>
2023-12-01 11:08:30 +0900

    flickr data 호출

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 
 export default function Gallery() {
+	const [Pics, setPics] = useState([]);
+
+	const fetchFlickr = async () => {
+		const flickr_api = '96b263310d877718514c269c64e06a63';
+		const baseURL = 'https://www.flickr.com/services/rest/?method=';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 500;
+		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
+
+		const data = await fetch(resultURL);
+		const json = await data.json();
+		setPics(json.photos.photo);
+	};
+
+	useEffect(() => {
+		fetchFlickr();
+	}, []);
+
 	return (
 		<Layout title={'Gallery'}>
-			<p>Gallery 전용 컨텐츠</p>
+			{Pics.map((pic, idx) => {
+				return (
+					<article key={pic.id}>
+						<h2>{pic.title}</h2>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
