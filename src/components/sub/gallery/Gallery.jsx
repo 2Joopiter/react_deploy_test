99174424e19d96297826a2ce78c9507d73a68e88
2joopiter <veryberrylike3@gmail.com>
2023-12-05 10:53:02 +0900

    flickr 검색 결과 없을 시 팝업창 출력

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -63,12 +63,15 @@ export default function Gallery() {
 
 		const data = await fetch(url);
 		const json = await data.json();
+
+		if (json.photos.photo.length === 0) {
+			return alert('해당 검색어의 결과값이 없습니다');
+		}
 		setPics(json.photos.photo);
 	};
 
 	useEffect(() => {
 		fetchFlickr({ type: 'user', id: myID.current });
-		//fetchFlickr({ type: 'search', keyword: '고양이' }); // '고양이' 키워드로 검색 타입 갤러리 호출
 	}, []);
 
 	return (
