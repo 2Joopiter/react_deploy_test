a2b57dbf0603c6cecb44024d3c0b7a55e17d4030
2joopiter <veryberrylike3@gmail.com>
2023-12-05 13:10:23 +0900

    framer motion으로 모션 속성 설정

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -1,12 +1,24 @@
 import './Modal.scss';
+import { AnimatePresence, motion } from 'framer-motion';
 
-//모달 컴포넌트 자체적으로 특정 state값에 따라서 자기 자신의 컨텐츠를 보여줄지 말지를 결정
-//부모 컴포넌트 기준에서 Modal 컴포넌트는 계속 마운트 되어 있는 상태이지만, State 값에 따라서 DOM 출력/미출력만 변경됨
+/*
+	AnimatePresence: 모션을 적용할 컴포넌트의 wrapping 컴포넌트 지정
+	- 자식요소의 모션이 끝날때까지 컴포넌트가 언마운트 되는 시점을 holding 처리
+	
+	motion: 모션을 걸고 싶은 JSX 컴포넌트에 연결해서 initial, animate, exit라는 속성으로 모션 수치값을 조절 가능
+	-initial: 모션이 일어나기 전 상태값 / animate: 모션이 일어날 때의 상태값 / exit: 해당 컴포넌트가 사라질 때의 상태값
+*/
 export default function Modal({ Open, setOpen, children }) {
 	return (
-		<>
+		<AnimatePresence>
 			{Open && (
-				<aside className='Modal'>
+				<motion.aside
+					className='Modal'
+					initial={{ opacity: 0, y: '-100%', scale: 0, rotate: -45 }}
+					animate={{ opacity: 1, y: '0%', scale: 1, rotate: 0 }}
+					exit={{ opacity: 0, y: '100%' }}
+					transition={{ duration: 0.5 }}
+				>
 					<div className='con'>{children}</div>
 					<span
 						onClick={() => {
@@ -15,8 +27,8 @@ export default function Modal({ Open, setOpen, children }) {
 					>
 						close
 					</span>
-				</aside>
+				</motion.aside>
 			)}
-		</>
+		</AnimatePresence>
 	);
 }
