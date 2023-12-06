b8872aaf13a651a98995060ed672def1af1bfd9b
2joopiter <veryberrylike3@gmail.com>
2023-12-06 10:12:01 +0900

    sass 변수값을 리액트에서 동적으로 제어하고 적용하는 흐름 주석 설명

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -102,6 +102,7 @@ export default function Gallery() {
 					</form>
 				</article>
 
+				{/* masonry는 처음 동작할 때 동적인 요소가 만들어지기 전에 미리 설정이 다 되어 있어야 하는데 gutter값을 scss에서 가져오게 되면 데이터를 가져오는 시간이 너무 늦어지게 됨. 따라서 react에서 제어하면 처음 마운트될 때 값이 들어가있으므로 훨씬 빠르게 처리 가능 */}
 				<section className='frameWrap' ref={refFrameWrap}>
 					<Masonry className={'frame'} options={{ transitionDuration: '0.5s', gutter: gap.current }}>
 						{Pics.length === 0 ? (
@@ -147,3 +148,18 @@ export default function Gallery() {
 		</>
 	);
 }
+
+/*
+	1. 일반 동적 데이터를 제외한 일반 정적인 컨텐츠가 렌더링 됨 (참조객체에 20이라는 상수값을 미리 담아놓음)
+	2. 정적인 JSX 요소가 일단 브라우저에 렌더링 완료 되었기 때문에 useEffect 실행 가능해짐 
+	3. useEffect 안쪽에서 미리 연결해놓은 참조객체 refFrameWrap에 접근 가능 (이때 refFrameWrap에 --gap 변수의 20이라는 값을 강제 적용)
+	-> 이때부터는 sass 파일에 --gap이라는 변수가 없더라도 react에서 동적으로 gap이라는 변수를 넣었기 때문에 활용 가능
+	4. 리액트에 동적으로 변수값을 적용해서 돔을 생성하고 나면 그 이후 scss가 해당 변수값을 읽어서 화면 
+	
+
+	<간소한 순서>
+	1. 처음에 gap이라는 참조객체값을 해석
+	2. 두번째 렌더링 타이밍에 useEffect가 실행이 되면서 참조객체에 담겨있는 section 요소에 강제로 gap 변수값을 적용
+	3. 세번째 렌더링 타임에 동적으로 나머지 flickr fetching 데이터에 의한 동적 요소가 출력되면서 그때 비로소 변수값이 적용된 sass 스타일링 적용
+	(paint)
+*/
