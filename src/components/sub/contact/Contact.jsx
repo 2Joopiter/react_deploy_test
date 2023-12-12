24a6dfa91f2f4beb2846e0a046626a5e5f9fbaa2
2joopiter <veryberrylike3@gmail.com>
2023-12-12 10:15:14 +0900

    contact 구조 주석 추가

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -3,11 +3,11 @@ import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
-	const [Index, setIndex] = useState(0);
-	const [Traffic, setTraffic] = useState(false);
-	const [View, setView] = useState(false);
+	const [Index, setIndex] = useState(0); // 버튼 클릭할때마다 화면이 재렌더링되어 순번에 맞게 재출력
+	const [Traffic, setTraffic] = useState(false); // 값을 반전시키면서 보이고 안 보이게 처리
+	const [View, setView] = useState(false); // 컴포넌트가 재렌더링되어 로드뷰화면/맵화면 전환 처리
 
-	const kakao = useRef(window.kakao);
+	const kakao = useRef(window.kakao); // 카카오 객체를 가져옴(index.html에 연결해둔 것)
 	const marker = useRef(null);
 	const mapInstance = useRef(null);
 	const viewFrame = useRef(null);
@@ -19,46 +19,61 @@ export default function Contact() {
 			latlng: new kakao.current.maps.LatLng(37.52506188634506, 126.9259552665427),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
 			imgSize: new kakao.current.maps.Size(232, 99),
-			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
 		},
 		{
 			title: '삼성역 코엑스',
 			latlng: new kakao.current.maps.LatLng(37.51100661425726, 127.06162026853143),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
 			imgSize: new kakao.current.maps.Size(232, 99),
-			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
 		},
 		{
 			title: '서울 시청',
 			latlng: new kakao.current.maps.LatLng(37.5662952, 126.9779451),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
 			imgSize: new kakao.current.maps.Size(232, 99),
-			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
-		},
-	]);
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
+		}
+	]); // 객체로 값을 담음
 
 	// 마커 인스턴스 생성
 	marker.current = new kakao.current.maps.Marker({
 		position: mapInfo.current[Index].latlng,
-		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt),
+		image: new kakao.current.maps.MarkerImage(
+			mapInfo.current[Index].imgSrc,
+			mapInfo.current[Index].imgSize,
+			mapInfo.current[Index].imgOpt
+		)
 	});
 
+	// 로드뷰 함수
 	const roadView = () => {
-		new kakao.current.maps.RoadviewClient().getNearestPanoId(mapInfo.current[Index].latlng, 100, (panoId) => {
-			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, mapInfo.current[Index].latlng);
-		});
+		new kakao.current.maps.RoadviewClient().getNearestPanoId(
+			mapInfo.current[Index].latlng,
+			100,
+			panoId => {
+				new kakao.current.maps.Roadview(viewFrame.current).setPanoId(
+					panoId,
+					mapInfo.current[Index].latlng
+				);
+			}
+		);
 	};
 
+	// 지도위치 갱신시키는 함수
 	const setCenter = () => {
 		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
 		roadView();
 	};
 
+	// 컴포넌트 마운트시 참조객체에 담아놓은 돔 프레임에 지도 인스턴트 출력 및 마커 생성
+	// 일일이 제어해야 하는 값을 state 정보값으로 한번에 제어 가능해짐
 	useEffect(() => {
 		mapFrame.current.innerHTML = '';
 		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, {
 			center: mapInfo.current[Index].latlng,
-			level: 3,
+			level: 3
 		});
 		marker.current.setMap(mapInstance.current);
 		setTraffic(false);
@@ -66,8 +81,17 @@ export default function Contact() {
 
 		roadView();
 
-		mapInstance.current.addControl(new kakao.current.maps.MapTypeControl(), kakao.current.maps.ControlPosition.TOPRIGHT);
-		mapInstance.current.addControl(new kakao.current.maps.ZoomControl(), kakao.current.maps.ControlPosition.RIGHT);
+		// 지도타입 컨트롤러 추가
+		mapInstance.current.addControl(
+			new kakao.current.maps.MapTypeControl(),
+			kakao.current.maps.ControlPosition.TOPRIGHT
+		);
+		// 지도 줌 컨트롤러 추가
+		mapInstance.current.addControl(
+			new kakao.current.maps.ZoomControl(),
+			kakao.current.maps.ControlPosition.RIGHT
+		);
+		// 휠로 맵 줌 기능 비활성화
 		mapInstance.current.setZoomable(false); // 마우스 휠로 맵 줌/아웃 기능 비활성화(true로 바꾸면 다시 활성화)
 
 		window.addEventListener('resize', setCenter);
@@ -84,18 +108,20 @@ export default function Contact() {
 		<Layout title={'Contact'}>
 			<div className='controlBox'>
 				<nav className='branch'>
-					{mapInfo.current.map((el, idx) => (
+					{mapInfo.current.map((el, idx) =>
+						//prettier-ignore
 						<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
 							{el.title}
 						</button>
-					))}
+					)}
 				</nav>
 				<nav className='info'>
+					{' '}
+					{/* 함수 거의 없고 state 값에 따른 제어 */}
 					<button
 						onClick={() => {
 							setTraffic(!Traffic);
-						}}
-					>
+						}}>
 						{Traffic ? 'Traffic OFF' : 'Traffic ON'}
 					</button>
 					<button onClick={() => setView(!View)}>{View ? 'map' : 'road view'}</button>
