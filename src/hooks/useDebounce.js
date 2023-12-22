fa403edb8da334aa912b99ed18861b6bb1b2b35b
2joopiter <veryberrylike3@gmail.com>
2023-12-22 11:48:01 +0900

    useDebounce 메모리 누수 오류 해결

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,21 +1,20 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
-export const useDebounce = (value, gap) => {
+export const useDebounce = (value, gap = 500) => {
+	const [Mounted, setMounted] = useState(true);
+
 	const [DebouncedVal, setDebouncedVal] = useState(value);
-	const eventBlocker = useRef(null); // setTimeout의 리턴값을 받을 참조객체
-
-	// 인수로 받은 state값이 변경될때마다 setTimeout 구문의 호출을 계속 초기화
-	// clearTimeout을 이용
+	const eventBlocker = useRef(null);
 
 	clearTimeout(eventBlocker.current);
 
-	// 아래 setTimeout에 의해서 원래 State이 0.5초 뒤에 무조건 변경이 되는 구조
-	// 만약 0.5초 안에 다시 value로 전달된 state가 전달되면 setTimeout의 리턴값을 초기화
-	// setTimeout의 리턴값을 clearTimeout으로 초기화시킴 (지연시간을 다시 0.5초로 리셋)
-
 	eventBlocker.current = setTimeout(() => {
-		setDebouncedVal(value);
+		Mounted && setDebouncedVal(value);
 	}, gap);
+
+	useEffect(() => {
+		return () => setMounted(false);
+	}, [Mounted]);
 
 	return DebouncedVal;
 };
