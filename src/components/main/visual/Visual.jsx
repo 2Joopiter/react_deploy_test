882ded2e3dc1f3955244708f3508a4dce6371552
2joopiter <veryberrylike3@gmail.com>
2023-12-29 11:09:00 +0900

    페이지네이션 기능 추가 및 스타일링

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,13 +1,24 @@
 import './Visual.scss';
-import 'swiper/css';
+import 'swiper/scss';
+import 'swiper/css/pagination';
+
 import { useSelector } from 'react-redux';
+import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function Visual() {
 	const { youtube } = useSelector(store => store.youtubeReducer);
 	return (
 		<figure className='Visual'>
-			<Swiper>
+			<Swiper
+				modules={[Pagination]}
+				pagination={{
+					clickable: true,
+					renderBullet: (index, className) => {
+						return `<span class=${className}>${index + 1}</span>`;
+					}
+				}}
+				loop={true}>
 				{youtube.map((vid, idx) => {
 					if (idx >= 5) return null;
 					return (
@@ -22,3 +33,11 @@ export default function Visual() {
 		</figure>
 	);
 }
+
+/*
+ 옵션값 설명
+ + slidesPerView: 한 화면에 몇개의 항목이 보이게 할 것인지. ex)2 = 한 화면에 2개 보임
+ + spaceBetween: 한 화면에 여러개의 항목이 보일 때 그 사이 간격값
+ + loop: 순환의 유무. true면 순환
+ + Pagination: 아래에 페이지버튼 
+*/
