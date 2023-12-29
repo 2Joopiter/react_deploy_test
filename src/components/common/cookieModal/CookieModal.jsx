e3bd29a66f11d8db8778db1bd7b298f8e2a0a4b4
2joopiter <veryberrylike3@gmail.com>
2023-12-29 09:20:24 +0900

    쿠키팝업 기본 UI 스타일링

--- src/components/common/cookieModal/CookieModal.jsx
+++ src/components/common/cookieModal/CookieModal.jsx
@@ -1,9 +1,20 @@
 import './CookieModal.scss';
 
-export default function CookieModal({ wid, ht }) {
+export default function CookieModal({ wid, ht, children }) {
 	return (
 		<aside
 			className='CookieModal'
-			style={{ width: wid, height: ht, marginLeft: -wid / 2, marginTop: -ht / 2 }}></aside>
+			style={{ width: wid, height: ht, marginLeft: -wid / 2, marginTop: -ht / 2 }}>
+			<div className='content'>{children}</div>
+
+			<div className='controls'>
+				<nav>
+					<input type='checkbox' />
+					<span>오늘하루 팝업보지 않기</span>
+				</nav>
+
+				<span>close</span>
+			</div>
+		</aside>
 	);
 }
