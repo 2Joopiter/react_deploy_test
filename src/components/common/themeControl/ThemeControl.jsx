9f18b6a3da971e1fc3baf81b7e14faaa71789e85
2joopiter <veryberrylike3@gmail.com>
2023-12-29 17:18:42 +0900

    useThrottle훅 추가 및 themeControll컴포넌트에서 changeTheme함수 throttle적용

--- src/components/common/themeControl/ThemeControl.jsx
+++ src/components/common/themeControl/ThemeControl.jsx
@@ -1,15 +1,12 @@
 import './ThemeControl.scss';
 import { useCookie } from '../../../hooks/useCookie';
 import { useCallback, useEffect, useRef } from 'react';
+import { useThrottle } from '../../../hooks/useThrottle';
 
 export default function ThemeControl() {
 	const inputEl = useRef(null);
 	const { setCookie, isCookie } = useCookie();
 
-	//초기 마운트시 쿠키가 있으면 쿠키에 등록된 값을 --pointColor변수에 할당하고
-	//그렇지 않으면 scss에 등록되어 있는 값을 --pointColor변수에 재할당
-	//어떤경우던 이후부터는 무조건 document.body.style.getPropertyValue로 현재 theme색상값 호출해서 변경가능
-	//변경된 색상값으로 input요소에 value색상도 변경처리
 	const getThemeColor = useCallback(() => {
 		isCookie('theme')
 			? document.body.style.setProperty(
@@ -23,9 +20,6 @@ export default function ThemeControl() {
 		inputEl.current.value = document.body.style.getPropertyValue('--pointColor');
 	}, [isCookie]);
 
-	//토글 버튼 클릭시마다
-	//현재 input요소에 선택된 value값으로 쿠키에 저장하고 css변수값에도 재할당
-	//그리고 input요소의 초기값도 다시 변경처리
 	const changeThemeColor = () => {
 		const color = inputEl.current.value;
 		document.body.style.setProperty('--pointColor', color);
@@ -33,19 +27,15 @@ export default function ThemeControl() {
 		inputEl.current.value = color;
 	};
 
-	//초기 마운트시에 컬러테마 쿠키값 유무에 따라 변수값 처리
+	const throttledChangeTheme = useThrottle(changeThemeColor, 300);
+
 	useEffect(() => {
 		getThemeColor();
 	}, [getThemeColor]);
 
 	return (
 		<nav className='ThemeControl'>
-			<input type='color' ref={inputEl} onChange={changeThemeColor} />
+			<input type='color' ref={inputEl} onChange={throttledChangeTheme} />
 		</nav>
 	);
 }
-
-/*
-  1.클릭이벤트 컬러팔레트에서 선택한 색상코드값을 쿠키로 저장
-  2.App마운트시 --pointColor에 등록된 value값을 쿠키에있는 값으로 변경처리
-*/
