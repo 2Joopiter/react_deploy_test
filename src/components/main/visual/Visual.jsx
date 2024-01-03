95af7347a0ea9326d652711fc042ecebd3ac9d64
2joopiter <veryberrylike3@gmail.com>
2024-01-03 16:48:38 +0900

    헌재 활성화 순번에 따른 좌우 썸네일 미리보기 출력

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -2,24 +2,31 @@ import './Visual.scss';
 import 'swiper/css';
 import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Visual() {
 	const { isSuccess, data } = useYoutubeQuery();
 	const [Index, setIndex] = useState(0);
+	const [prevIndex, setPrevIndex] = useState(4);
+	const [nextIndex, setNextIndex] = useState(1);
 
 	const swiperOpt = useRef({
 		loop: true,
 		slidesPerView: 1,
 		spaceBetween: 50,
 		centeredSlides: true,
-		onSwiper: swiper => swiper.slideNext(300),
+		//onSwiper: swiper => swiper.slideNext(300),
 		onSlideChange: swiper => setIndex(swiper.realIndex),
 		breakpoints: {
 			1000: { slidesPerView: 2 },
 			1400: { slidesPerView: 3 }
 		}
 	});
+
+	useEffect(() => {
+		Index === 0 ? setPrevIndex(4) : setPrevIndex(Index - 1);
+		Index === 4 ? setNextIndex(0) : setNextIndex(Index + 1);
+	}, [Index]);
 
 	return (
 		<figure className='Visual'>
@@ -54,6 +61,25 @@ export default function Visual() {
 						);
 					})}
 			</Swiper>
+
+			<nav className='preview'>
+				{isSuccess && (
+					<>
+						<p className='prevBox'>
+							<img
+								src={data[prevIndex].snippet.thumbnails.default.url}
+								alt={data[prevIndex].snippet.title}
+							/>
+						</p>
+						<p className='nextBox'>
+							<img
+								src={data[nextIndex].snippet.thumbnails.default.url}
+								alt={data[nextIndex].snippet.title}
+							/>
+						</p>
+					</>
+				)}
+			</nav>
 		</figure>
 	);
 }
