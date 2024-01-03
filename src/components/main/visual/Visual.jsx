25c7d83bc25d779ebe8b5551bdfbc083ee6f09c9
2joopiter <veryberrylike3@gmail.com>
2024-01-03 14:52:58 +0900

    swiper option객체 설정

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -2,13 +2,23 @@ import './Visual.scss';
 import 'swiper/css';
 import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useRef, useState } from 'react';
 
 export default function Visual() {
 	const { isSuccess, data } = useYoutubeQuery();
+	const swiperOpt = useRef({
+		loop: true,
+		slidesPerView: 1,
+		centeredSlides: true,
+		breakpoints: {
+			1000: { slidesPerView: 2, spaceBetween: 50 },
+			1400: { slidesPerView: 3, spaceBetween: 50 }
+		}
+	});
 	console.log(data);
 	return (
 		<figure className='Visual'>
-			<Swiper>
+			<Swiper {...swiperOpt.current}>
 				{isSuccess &&
 					data.map((el, idx) => {
 						if (idx >= 5) return null;
