b52307d4613a9507e95f9de1860a0b2ca3ccdb8e
2joopiter <veryberrylike3@gmail.com>
2024-01-03 15:14:33 +0900

    swiper 옵션 수정

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -9,16 +9,17 @@ export default function Visual() {
 	const swiperOpt = useRef({
 		loop: true,
 		slidesPerView: 1,
+		spaceBetween: 50,
 		centeredSlides: true,
 		onSwiper: swiper => {
 			swiper.slideNext(300);
 		},
 		breakpoints: {
-			1000: { slidesPerView: 2, spaceBetween: 50 },
-			1400: { slidesPerView: 3, spaceBetween: 50 }
+			1000: { slidesPerView: 2 },
+			1400: { slidesPerView: 3 }
 		}
 	});
-	console.log(data);
+
 	return (
 		<figure className='Visual'>
 			<div className='txtBox'></div>
