5cbb787eaeda89c0af85506589ed05f3e02b90ed
2joopiter <veryberrylike3@gmail.com>
2024-01-04 13:19:47 +0900

    코드 수정

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,17 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
 import './Btns.scss';
 import Anime from '../../../asset/anime';
+import { useRef, useState, useEffect } from 'react';
 import { useThrottle } from '../../../hooks/useThrottle';
 
 export default function Btns() {
 	const [Num, setNum] = useState(0);
-
-	const secs = useRef(null);
 	const wrap = useRef(null);
+	const secs = useRef(null);
 	const btns = useRef(null);
-	const baseLine = useRef(-window.innerHeight / 3); // 현재 섹션의 컨텐츠가 3분의 1 이상 보일 때 버튼 활성화. 절반 이상일 때 넘어가게 하려면 2로.
+	const baseLine = useRef(-window.innerHeight / 3); //현재 섹션의 컨텐츠가 3분의 1 이상 보여야 버튼 활성화. 절반일 땐 2
 
 	const activation = () => {
+		console.log('activation');
 		const scroll = wrap.current.scrollTop;
 
 		secs.current.forEach((sec, idx) => {
@@ -30,11 +30,11 @@ export default function Btns() {
 		setNum(secs.current.length);
 
 		wrap.current.addEventListener('scroll', throttledActivation);
-		return () => wrap.current.removeEventListener('scroll', activation);
+		return () => wrap.current.removeEventListener('scroll', throttledActivation);
 	}, [throttledActivation]);
 
 	return (
-		<div className='Btns' ref={btns}>
+		<ul className='Btns' ref={btns}>
 			{Array(Num)
 				.fill()
 				.map((_, idx) => {
@@ -43,6 +43,7 @@ export default function Btns() {
 							key={idx}
 							className={idx === 0 ? 'on' : ''}
 							onClick={() => {
+								//new Anime(선택자, {속성명1:속성값2, 속성명2:속성값2}, {duration:속도, easeType:가속도, callback:컴플릭함수})
 								new Anime(
 									wrap.current,
 									{ scroll: secs.current[idx].offsetTop },
@@ -51,12 +52,6 @@ export default function Btns() {
 							}}></li>
 					);
 				})}
-		</div>
+		</ul>
 	);
 }
-
-/*
-window.scrollY: 브라우저를 스크롤 할 때마다 스크롤 되고 있는 거리값을 담고 있는 property (동적인 값)
-DOM.scrollY: DOM 요소를 스크롤 할 때마다 스크롤 되고 있는 거리값을 담고 있는 property
-DOM.offsetTop: 문서에서 해당 DOM 요소의 세로 위치값을 담고 있는 property (정적인 값)
-*/
