dc78cc37ba6fe9477ce5d8e6087328e19f92aa4a
2joopiter <veryberrylike3@gmail.com>
2024-01-04 11:24:16 +0900

    scrollTop, offsetTop의 차이점

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,9 +1,20 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './Btns.scss';
 
 export default function Btns() {
 	const [Index, setIndex] = useState(0);
+	const btns = useRef(null);
+	const secs = useRef(null);
+	const wrap = useRef(null);
 	const num = useRef(4);
+
+	useEffect(() => {
+		wrap.current = document.querySelector('.wrap');
+		secs.current = document.querySelectorAll('.myScroll');
+		wrap.current.addEventListener('scroll', (e) => {
+      
+    });
+	}, []);
 
 	return (
 		<div className='Btns'>
@@ -15,3 +26,9 @@ export default function Btns() {
 		</div>
 	);
 }
+
+/*
+window.scrollY: 브라우저를 스크롤 할 때마다 스크롤 되고 있는 거리값을 담고 있는 property (동적인 값)
+DOM.scrollY: DOM 요소를 스크롤 할 때마다 스크롤 되고 있는 거리값을 담고 있는 property
+DOM.offsetTop: 문서에서 해당 DOM 요소의 세로 위치값을 담고 있는 property (정적인 값)
+*/
