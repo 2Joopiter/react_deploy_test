136eee29d74df95f37b239d1374211b2ad2d2751
2joopiter <veryberrylike3@gmail.com>
2024-01-05 15:13:38 +0900

    특정 스크롤 위치값 넘어가면 btnTop보이도록 설정

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,16 +1,16 @@
 import Anime from '../asset/anime';
-import { useEffect, useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
-export function useScroll(frame) {
-	// 스크롤이벤트가 발생하는 프레임 요소
+export function useScroll(frame = '.wrap') {
+	//스크롤이벤트가 발생하는 프레임 요소
 	const scrollFrame = useRef(null);
 
-	// 특정 위치로 스크롤 이동 메서드
+	//특정위치로 스크롤 이동 메서드
 	const scrollTo = targetPos => {
 		new Anime(scrollFrame.current, { scroll: targetPos });
 	};
 
-	// 실시간 scroll값 반환 메서드
+	//실시간 scroll값 반환 메서드
 	const getCurrentScroll = () => {
 		const scroll = scrollFrame.current.scrollTop;
 		return scroll;
@@ -20,6 +20,5 @@ export function useScroll(frame) {
 		scrollFrame.current = document.querySelector(frame);
 	}, [frame]);
 
-	//scrollTo 함수를 비구조화 할당으로 뽑아내기 위해서 객체로 묶어 반환
 	return { scrollTo, getCurrentScroll, scrollFrame };
 }
