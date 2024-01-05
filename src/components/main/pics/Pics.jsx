644ea456aebc22df15276e4623e4a6aa9f22514c
2joopiter <veryberrylike3@gmail.com>
2024-01-05 17:18:46 +0900

    스크롤 커스텀 훅 오류 수정

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -1,5 +1,29 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useScroll } from '../../../hooks/useScroll';
 import './Pics.scss';
-
 export default function Pics() {
-	return <section className='Pics myScroll'>Pics</section>;
+	console.log('Pics');
+	const [Frame, setFrame] = useState(null);
+	const thisEl = useRef(null);
+	const boxEl = useRef(null);
+	const { getCurrentScroll } = useScroll(Frame);
+
+	const handleScroll = useCallback(() => {
+		const scroll = getCurrentScroll(thisEl.current, -window.innerHeight / 2);
+		scroll >= 0 && (boxEl.current.style.transform = `translateX(${scroll}px)`);
+	}, [getCurrentScroll]);
+
+	useEffect(() => {
+		setFrame(thisEl.current?.closest('.wrap'));
+	}, []);
+
+	useEffect(() => {
+		Frame?.addEventListener('scroll', handleScroll);
+		return () => Frame?.removeEventListener('scroll', handleScroll);
+	}, [Frame, handleScroll]);
+	return (
+		<section className='Pics myScroll' ref={thisEl}>
+			<div className='box' ref={boxEl}></div>
+		</section>
+	);
 }
