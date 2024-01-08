34b552bddf56c47b947db9ccae64abc1774954bf
2joopiter <veryberrylike3@gmail.com>
2024-01-08 12:43:57 +0900

    svg의 viewBox옵션을 조절해서 stoke-width에 값에 따른 화면 조절

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -1,20 +1,20 @@
 import './Banner.scss';
+import { useRef } from 'react';
 import { useScroll } from '../../../hooks/useScroll';
-import { useCallback, useRef } from 'react';
 
 export default function Banner() {
 	const boxEl = useRef(null);
-
-	// 순서1 - useScroll 훅으로 전달할 해당 컴포넌트에서의 커스텀 스크롤 함수 정의
 	const customHandleScroll = scroll => {
-		boxEl.current.style.transform = `rotate(${scroll / 5}deg) scale(${1 + scroll / 400})`;
-		boxEl.current.style.opacity = 1 - scroll / 400;
+		if (scroll >= 0) {
+			boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
+			boxEl.current.style.opacity = 1 - scroll / 400;
+		} else {
+			boxEl.current.style.transform = `rotate(0deg) scale(1)`;
+			boxEl.current.style.opacity = 1;
+		}
 	};
 
-	// 순서2 - 커스텀스크롤 함수를 useScroll 호출시 전달
 	const { refEl } = useScroll(customHandleScroll);
-
-	useScroll();
 	return (
 		<section className='Banner myScroll' ref={refEl}>
 			<div className='box' ref={boxEl}></div>
--- src/components/main/mainWrap/MainWrap.jsx
+++ src/components/main/mainWrap/MainWrap.jsx
@@ -1,9 +1,10 @@
+import './MainWrap.scss';
 import Banner from '../banner/Banner';
 import Info from '../info/Info';
 import Pics from '../pics/Pics';
 import Visual from '../visual/Visual';
 import Btns from '../btns/Btns';
-import './MainWrap.scss';
+import Illust from '../illust/Illust';
 
 export default function MainWrap() {
 	return (
@@ -11,6 +12,7 @@ export default function MainWrap() {
 			<Visual />
 			<Info />
 			<Pics />
+			<Illust />
 			<Banner />
 			<Btns />
 		</div>
