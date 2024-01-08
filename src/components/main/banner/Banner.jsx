1694af45f714b3b35f05ed27050325f5d245be39
2joopiter <veryberrylike3@gmail.com>
2024-01-08 10:21:50 +0900

    useScroll안쪽에서 자체적으로 빈 참조객체 만들어서 부모 컴포넌트에 반환해줌

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -3,14 +3,13 @@ import { useScroll } from '../../../hooks/useScroll';
 import { useCallback, useEffect, useRef, useState } from 'react';
 
 export default function Banner() {
-	const refBanner = useRef(null);
 	const boxEl = useRef(null);
 
 	//frame을 전달하면 getCurrentScroll 값을 가져옴
-	const { getCurrentScroll, Frame } = useScroll();
+	const { getCurrentScroll, Frame, refEl } = useScroll(); // 순서3 - useScroll에서 내보낸 함수를 가져옴
 
 	const handleScroll = useCallback(() => {
-		const scroll = getCurrentScroll(refBanner.current, -window.innerHeight / 2);
+		const scroll = getCurrentScroll(refEl.current, -window.innerHeight / 2);
 		if (scroll >= 0) {
 			boxEl.current.style.transform = `rotate(${scroll / 5}deg) scale(${1 + scroll / 400})`;
 			boxEl.current.style.opacity = 1 - scroll / 400;
@@ -26,7 +25,8 @@ export default function Banner() {
 
 	useScroll();
 	return (
-		<section className='Banner myScroll' ref={refBanner}>
+		// 순서4 - 원하는 요소에 빈 참조객체 연결
+		<section className='Banner myScroll' ref={refEl}>
 			<div className='box' ref={boxEl}></div>
 		</section>
 	);
--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,18 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Anime from '../asset/anime';
 
 //useScroll훅을 처음 초기화할때 무조건 인수로 state에 담겨있는 ScrollFrame요소를 전달 (중요)
-export function useScroll(scrollFrame) {
+export function useScroll() {
+	// 순서1 - 커스텀훅 안쪽에서 자체적으로 빈 참조객체 생성
 	const [Frame, setFrame] = useState(null);
+	// 빈 참조객체를 내보내서 부모요소에서 연결해서 쓸 수 있게끔 만듦
+	const refEl = useRef(null);
 
 	const scrollTo = targetPos => {
 		Frame && new Anime(Frame, { scroll: targetPos });
 	};
 
 	//getCurrentScroll(호출하는 부모프레임요소, 기준점 보정값)
-	const getCurrentScroll = (selfEl, baseLine = 0) => {
+	const getCurrentScroll = (baseLine = 0) => {
 		const scroll = Frame?.scrollTop - baseLine;
-		const modifiedScroll = scroll - selfEl?.offsetTop;
+		// 순서 5 - 부모 컴포넌트에서 참조객체 연결된 값을 hook 내부적으로 활용
+		const modifiedScroll = scroll - refEl.current?.offsetTop;
 		return modifiedScroll;
 	};
 
@@ -21,5 +25,6 @@ export function useScroll(scrollFrame) {
 	}, []);
 	// .wrap 요소는 바뀌는 요소가 아니기때문에(최상위 document) querySelector를 써도 됨 + 내부적인 부분은 변경될때마다 setFrame에 의해 변경 가능
 
-	return { scrollTo, getCurrentScroll, Frame };
+	// 순서2 - 부모에서 해당 참조객체를 활용할 수 있도록 리턴해줌
+	return { scrollTo, getCurrentScroll, Frame, refEl };
 }
